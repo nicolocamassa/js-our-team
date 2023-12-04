27e86988bdcaac039d4bee795d141db878ada4a4
nicolocamassa <camassanicolo03@gmail.com>
2023-12-04 15:47:16 +0100

    [UPDATE] added object in array

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let members = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    }
+]
