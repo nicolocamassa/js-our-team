6d1760e889fef9cf3d2540e11f288eb8386632e8
nicolocamassa <camassanicolo03@gmail.com>
2023-12-04 16:00:26 +0100

    [UPDATE] change all keys value

--- js/script.js
+++ js/script.js
@@ -7,38 +7,38 @@ let members = [
     },
 
     {
-        name: "Wayne",
-        surname: "Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Angela",
+        surname: "Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
     },
 
     {
-        name: "Wayne",
-        surname: "Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Walter",
+        surname: "Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
     },
 
     {
-        name: "Wayne",
-        surname: "Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Angela",
+        surname: "Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
     },
 
     {
-        name: "Wayne",
-        surname: "Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Scott",
+        surname: "Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
     },
 
     {
-        name: "Wayne",
-        surname: "Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Barbara",
+        surname: "Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
